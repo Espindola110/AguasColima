c287e85fd9c0f37b8c20242ae9ce1553d64e31d3
Luis Fernando Espindola Silva <lespindola@ucol.mx>
2019-09-02 10:15:15 -0500

    Thesis

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
 import { HomePage } from './home.page';
 
@@ -20,4 +20,11 @@ import { HomePage } from './home.page';
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule {
+  constructor(private router: Router){}
+    go(){
+      this.router.navigate(['admin'])
+    }
+  
+}
+
